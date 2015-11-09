f736f1647177531a12369456b25301e9f01ddab2
sergeyeremeev <13.eremeev.sv@gmail.com>
2015-11-10 01:14:25 +0300

    modify gulpfile and add basic file structure as well as basic index.html structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,15 @@
 var gulp = require('gulp'),
+    del = require('del'),
     minifyHTML = require('gulp-minify-html'),
     uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     minifyCSS = require('gulp-minify-css'),
-    imageop = require('gulp-image-optimization'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
-    del = require('del');
+    imageop = require('gulp-image-optimization');
 
 // clean task to clean build folder before building an entire app
 gulp.task('clean', function (cb) {
     del('build/**/*', cb);
-});
-
-// minify scripts
-gulp.task('scripts', function () {
-    gulp.src('dev/**/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('build'))
-        .pipe(reload({ stream:true }));
-});
-
-// minify styles
-gulp.task('styles', function () {
-    gulp.src('dev/**/*.css')
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('build'))
-        .pipe(reload({ stream:true }));
 });
 
 // minify html
@@ -40,6 +24,29 @@ gulp.task('html', function() {
     .pipe(gulp.dest('build'));
 });
 
+// minify scripts
+gulp.task('scripts', function () {
+    gulp.src('dev/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
+// compile sass to css
+gulp.task('sass', function () {
+    gulp.src('dev/**/*.scss')
+        .pipe(sourcemaps.init())
+            .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build')); 
+});
+
+// minify styles
+gulp.task('styles', ['sass'], function () {
+    gulp.src('dev/**/*.css')
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('build'));
+});
+
 // optimize images
 gulp.task('images', function(cb) {
     gulp.src(['dev/**/*.png','dev/**/*.jpg','dev/**/*.gif','dev/**/*.jpeg'])
@@ -52,16 +59,9 @@ gulp.task('images', function(cb) {
 });
 
 // watch task
-gulp.task('serve', function () {
-    browserSync({
-        port: 8000,
-        server: {
-            baseDir: 'build'
-        }
-    });
-
+gulp.task('watch', function () {
     gulp.watch('dev/**/*.css', ['styles']);
-    gulp.watch('dev/**/*.html', ['html-critical']);
+    gulp.watch('dev/**/*.html', ['html']);
     gulp.watch('dev/**/*.js', ['scripts']);
 });
 
